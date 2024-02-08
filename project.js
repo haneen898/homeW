15c2a431b69c7c0eb11a35877347aa4baeb44f75
haneen898 <haneenmansoor898@gmail.com>
2024-02-08 22:29:52 +0300

    first commit

---
+++ project.js
@@ -0,0 +1,125 @@
+// // < --- hm ---->
+// var myAge = 18;
+// if (myAge >= 18 ){
+//   console.log('you are eligible for votin' )
+// }else {
+//   console.log('you are not eligible for votin');
+// }
+
+
+
+
+// var day ;
+// var dayNumber ;
+// getDayOfWeek(dayNumber){
+//     //   getDayOfWeek(7);
+//  switch (dayNumber) {
+//     case 1:
+//         day = "Sunday";
+//         break;
+//     case 2:
+//         day = "Monday";
+//         break;
+//     case 3:
+//         day = "Tuesday";
+//         break;
+//     case 4:
+//         day = "Wednesday";
+//         break;
+//     case 5:
+//         day = "Thursday";
+//         break;
+//     case 6:
+//         day = "Friday";
+//         break;
+//     case 7:
+//         day = "Saturday";
+//         break;
+ 
+//     default:
+//         day = "Invalid day number"; 
+//         break;
+//  }
+
+
+// }
+
+
+  var day
+ function getDayOfWeek(dayNumber){
+    switch (dayNumber) {
+        case 1:
+          day = 'Sunday';
+          onsole.log(day);
+          break;
+        case 2:
+          day = 'Monday';
+          onsole.log(day);
+          break;
+        case 3:
+          day = 'Tuesday';
+          onsole.log(day);
+          break;
+        case 4:
+          day = 'Wednesday';
+          onsole.log(day);
+          break;
+        case 5:
+          day = 'Thursday';
+          onsole.log(day);
+          break;
+        case 6:
+          day = 'Friday';
+          onsole.log(day);
+          break;
+        case 7:
+          day = 'Saturday';
+          console.log(day);
+          break;
+    
+        default:
+          day = 'Invalid day number';
+          break;
+      }
+     }
+  getDayOfWeek(7);
+
+
+
+// // // <-- hm2 --->
+
+// var i = 1;
+// while(i<6){
+//   console.log(i);
+//   i++;
+// }
+
+
+
+// // // <---hm3--->
+
+// for (var i = 1; i <= 10; i++) {
+//     if(i % 2 === 0) {
+//       console.log(i);
+//     }
+// }
+
+
+
+// // // <----hm5----->
+
+// var num1 = 23;
+// var num2 = 45;
+// function sumTwo(num1,num2){
+//     console.log(num1+num2);
+//   }
+//   sumTwo(num1,num2)
+
+
+// <---hm -->
+
+// var fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grapes'];
+// fruits.forEach( fruit => {
+//     console.log(fruit);
+//       }
+// );
