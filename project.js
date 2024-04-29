bfe7a72c2cadee763928efc2b999415899fb3551
haneen898 <haneenmansoor898@gmail.com>
2024-04-29 11:06:07 +0300

    week

--- project.js
+++ project.js
@@ -1,48 +1,15 @@
-// // < --- hm ---->
-// var myAge = 18;
-// if (myAge >= 18 ){
-//   console.log('you are eligible for votin' )
-// }else {
-//   console.log('you are not eligible for votin');
-// }
+
+var myAge = 18;
+if (myAge >= 18 ){
+  console.log('you are eligible for votin' )
+}else {
+  console.log('you are not eligible for votin');
+}
 
 
 
 
-// var day ;
-// var dayNumber ;
-// getDayOfWeek(dayNumber){
-//     //   getDayOfWeek(7);
-//  switch (dayNumber) {
-//     case 1:
-//         day = "Sunday";
-//         break;
-//     case 2:
-//         day = "Monday";
-//         break;
-//     case 3:
-//         day = "Tuesday";
-//         break;
-//     case 4:
-//         day = "Wednesday";
-//         break;
-//     case 5:
-//         day = "Thursday";
-//         break;
-//     case 6:
-//         day = "Friday";
-//         break;
-//     case 7:
-//         day = "Saturday";
-//         break;
- 
-//     default:
-//         day = "Invalid day number"; 
-//         break;
-//  }
 
-
-// }
 
 
   var day
@@ -86,40 +53,40 @@
 
 
 
-// // // <-- hm2 --->
-
-// var i = 1;
-// while(i<6){
-//   console.log(i);
-//   i++;
-// }
 
 
-
-// // // <---hm3--->
-
-// for (var i = 1; i <= 10; i++) {
-//     if(i % 2 === 0) {
-//       console.log(i);
-//     }
-// }
+var i = 1;
+while(i<6){
+  console.log(i);
+  i++;
+}
 
 
 
-// // // <----hm5----->
-
-// var num1 = 23;
-// var num2 = 45;
-// function sumTwo(num1,num2){
-//     console.log(num1+num2);
-//   }
-//   sumTwo(num1,num2)
 
 
-// <---hm -->
+for (var i = 1; i <= 10; i++) {
+    if(i % 2 === 0) {
+      console.log(i);
+    }
+}
 
-// var fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grapes'];
-// fruits.forEach( fruit => {
-//     console.log(fruit);
-//       }
-// );
+
+
+
+
+var num1 = 23;
+var num2 = 45;
+function sumTwo(num1,num2){
+    console.log(num1+num2);
+  }
+  sumTwo(num1,num2)
+
+
+
+
+var fruits = ['Apple', 'Banana', 'Orange', 'Mango', 'Grapes'];
+fruits.forEach( fruit => {
+    console.log(fruit);
+      }
+);
